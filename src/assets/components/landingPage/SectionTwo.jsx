ced0af865db92040955bc6778f051ec6f74cc02f
Vipin <97426806+vpnbeni@users.noreply.github.com>
2025-01-18 13:13:50 +0530

    df

--- src/assets/components/landingPage/SectionTwo.jsx
+++ src/assets/components/landingPage/SectionTwo.jsx
@@ -5,10 +5,13 @@ const SectionTwo = () => {
   return (
     <div className="w-full bg-[#001c38] flex flex-col items-center font-light">
       <div className="text-center text-xl text-white px-4 lg:px-[20%] py-10">
-        Established in 2009, we are a privately-owned independent connection
-        provider that specialises in providing turnkey high voltage solutions to
-        a wide range of markets nationwide, including commercial, industrial,
-        renewable, new connections and rail.
+        Founded in 2003 by a team of dedicated engineers with over 20 years of
+        experience in power and infrastructure development in India, Apstechs UK
+        Limited has grown into a leading technology company in the UK. Our
+        journey from a visionary start up to a robust, fast-growing enterprise
+        is a testament to our commitment to excellence and innovation. With a
+        solid financial foundation and a vast pool of expertise, we now deliver
+        solutions that exceed even our founders’ most ambitious aspirations.
       </div>
       <div
         className={`w-fit bg-primary text-lightBlue mb-6 rounded-full p-4 border hover:text-dark hover:bg-white cursor-pointer `}
