0a1426898d1e9e8c6566351996194d5338b9d994
Vipin <97426806+vpnbeni@users.noreply.github.com>
2025-01-21 08:47:18 +0530

    sdaf

--- src/assets/components/layout/Header.jsx
+++ src/assets/components/layout/Header.jsx
@@ -1,57 +1,109 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    subItems: [],
+  },
+  {
+    name: "About",
+    link: "/about",
+    subItems: [
+      { name: "Meet our team", link: "/about/meet-our-team" },
+      { name: "Sustainability", link: "/about/sustainability" },
+    ],
+  },
+  {
+    name: "Services",
+    link: "/services",
+    subItems: [
+      { name: "Plant Engineering", link: "/services/plant-engineering" },
+      { name: "Cable Solution", link: "/services/cable-solution" },
+      { name: "Earthing Solution", link: "/services/earthing-solution" },
+      {
+        name: "Battery Storage Integration",
+        link: "/services/battery-storage-integration",
+      },
+      { name: "Protection Design", link: "/services/protection-design" },
+      { name: "Power System Studies", link: "/services/power-system-studies" },
+    ],
+  },
+  {
+    name: "Case Studies",
+    link: "/case-studies",
+    subItems: [],
+  },
+  {
+    name: "Careers",
+    link: "/careers",
+    subItems: [
+      { name: "Working for APSTECHS", link: "/careers/working-for-apstechs" },
+      { name: "Vacancies", link: "/careers/vacancies" },
+      { name: "Safety first Culture", link: "/careers/safety-first-culture" },
+      {
+        name: "Diversity and Inclusion",
+        link: "/careers/diversity-and-inclusion",
+      },
+      { name: "Family first", link: "/careers/family-first" },
+      { name: "Charity", link: "/careers/charity" },
+      {
+        name: "Learning and development",
+        link: "/careers/learning-and-development",
+      },
+    ],
+  },
+];
+
 const Header = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  // Handle scroll event
-  const handleScroll = () => {
-    if (window.scrollY > window.innerHeight) {
-      setIsScrolled(true);
-    } else {
-      setIsScrolled(false);
-    }
-  };
-
-  // Add event listener on mount, clean up on unmount
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const [activeTab, setActiveTab] = useState("");
 
   return (
-    <div
-      className={`w-full h-[96px] fixed z-[1000] top-0 bg-[#f0f8ff] bg-opacity-80 flex justify-between items-center px-[10%] gap-10 transition-transform duration-300 ${
-        isScrolled ? "-translate-y-[96px]" : "translate-y-0"
-      }`}
-    >
-      <Link to={"/"}>
-        <img className="h-[80px]" src={"/assets/header/logo.png"} alt="Logo" />
+    <div className="w-full h-[96px] fixed z-[1000] top-0 bg-[#f0f8ff] bg-opacity-80 flex justify-between items-center px-[10%] gap-10 transition-transform duration-300">
+      <Link to="/">
+        <img className="h-[80px]" src="/assets/header/logo.png" alt="Logo" />
       </Link>
-      <div className="w-fit flex justify-center gap-8">
-        {[
-          { name: "Home", link: "/" },
-          { name: "About", link: "/about" },
-          { name: "Services", link: "/services" },
-          { name: "Case Studies", link: "/case-studies" },
-          { name: "Careers", link: "/careers" },
-        ].map((item, index) => {
-          return (
+
+      <nav className="hidden lg:flex gap-8">
+        {navItems.map((item, index) => (
+          <div
+            key={index}
+            className="relative group"
+            onMouseEnter={() =>
+              item?.subItems?.length > 0 && setActiveTab(item?.name)
+            }
+          >
             <Link
               to={item.link}
-              key={index}
-              className="text-dark hover:text-secondary cursor-pointer text-lg font-semibold"
+              className="text-gray-700 text-lg flex gap-1 items-center hover:text-gray-900 font-medium"
             >
               {item.name}
             </Link>
-          );
-        })}
+
+            {item?.subItems?.length > 0 && activeTab === item?.name && (
+              <div
+                onMouseLeave={() => setActiveTab("")}
+                className="absolute left-0 mt-2 w-fit py-3 bg-white border-t-2 border-primary shadow-lg block"
+              >
+                <ul className="text-sm text-gray-700">
+                  {item?.subItems.map((subItem, subIndex) => (
+                    <li
+                      key={subIndex}
+                      className="px-4 py-2 hover:bg-gray-100 text-nowrap cursor-pointer hover:border-l-4 border-primary"
+                    >
+                      <Link to={subItem.link}>{subItem.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
         <button className="cursor-pointer border-2 border-dark rounded-lg px-3 hover:bg-dark hover:text-white text-dark">
           Contact Us
         </button>
-      </div>
+      </nav>
     </div>
   );
 };
