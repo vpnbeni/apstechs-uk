f4bf2a92979e819b75addf5285bfd24279ac114b
Vipin <97426806+vpnbeni@users.noreply.github.com>
2025-01-26 13:09:40 +0530

    df

--- src/assets/components/common/heading/SectionHeading.jsx
+++ src/assets/components/common/heading/SectionHeading.jsx
@@ -4,9 +4,7 @@ const SectionHeading = ({ heading, clasName }) => {
   return (
     <h1
       className={`w-fit font-[500] px-3 py-[-1px] tracking-[.25em] uppercase text-center ${
-        clasName
-          ? clasName
-          : "bg-secondary text-white text-[20px] sm:text-[24px] lg:text-[28px] xl:text-[32px] tracking-[.25em]"
+        clasName ? clasName : "bg-secondary text-white  text-[16px] "
       }`}
     >
       {heading}
